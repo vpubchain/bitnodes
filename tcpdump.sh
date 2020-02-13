b4157d8d48b9c9b9c79fcf3ae3267616c7bc8eaf
root <lixu_11@yeah.net>
2020-02-13 17:26:54 +0800

    tcpdump

--- tcpdump.sh
+++ tcpdump.sh
@@ -1,4 +1,1 @@
-tcpdump -i ens33 -w %s.ens33.pcap -v -n -G 2 -B 65536 -Z root > ens33 2>&1 &
-tcpdump -i lo -w %s.lo.pcap -v -n -G 2 -B 65536 -Z root 'tcp and port 9050' > lo 2>&1 &
-#tcpdump -i [ens33] -w %s.[ens33.pcap -v -n -G 2 -B 65536 -Z [root] 'tcp and not src host [IP_ADDRESS] and not src host [IPV6_ADDRESS]' > [ens33] 2>&1 &
-#tcpdump -G 60 -Z root -w %Y_%m%d_%H%M_%S.pcap &
+tcpdump -i eth0 -w %s.eth0.pcap -v -n -G 2 -B 65536 -Z root > eth0 2>&1 &
